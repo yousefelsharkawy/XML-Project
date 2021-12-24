cc6ef2be5a2becac08b18db8e2d610f9df9f3f5c
0ssamaak0 <0ssamaak0@gmail.com>
2021-12-24 10:09:03 +0200

    Final Version

--- App.java
+++
@@ -1,9 +0,0 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-
-public class App {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new FileReader("./test.xml"));
-        br.close();
-    }
-}
